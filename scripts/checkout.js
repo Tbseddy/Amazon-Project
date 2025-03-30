29758f28108402518231ba49a6a483155f30b106
Olalekan Bakare <peterubakare@gmail.com>
2025-03-30 05:54:35 +0100

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
 
 import {products} from '../data/products.js';
 
@@ -118,7 +118,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
             html +=
         `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio"
                     ${isChecked ? 'checked' : ''}
                 class="delivery-option-input"
@@ -157,3 +157,11 @@ document.querySelectorAll('.js-delete-link')
             container.remove();
         });
     });
+
+    document.querySelectorAll('.js-delivery-option')
+        .forEach((element) => {
+            element.addEventListener('clicks', () => {
+                const {productId, deliveryOptionId} = element.dataset;
+                updateDeliveryOption(productId, deliveryOptionId);
+            });
+        });
